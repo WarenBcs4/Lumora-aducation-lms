86154708ecd33630d7a07097eeec402562eaafce
unknown <waren9505@gmail.com>
2025-10-06 23:28:51 +0300

    farebase confugaration

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,18 +2,21 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyAOcVb6k3VTGYe2H8AWEhzW0qRxKq7DTtQ",
+  authDomain: "lumora-elearning.firebaseapp.com",
+  projectId: "lumora-elearning",
+  storageBucket: "lumora-elearning.firebasestorage.app",
+  messagingSenderId: "540009869527",
+  appId: "1:540009869527:web:cb7f07932086b10ede4092",
+  measurementId: "G-DY4D1PNSPG"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
 export default app;
